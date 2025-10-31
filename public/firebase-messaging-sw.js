0f3b0202ee8cd468d90de2c757fd5e0cf347fd43
mohammedsayed30 <124407113+mohammedsayed30@users.noreply.github.com>
2025-10-31 12:42:32 +0200

    Update firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,12 +2,12 @@ importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js");
 importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js");
 
 firebase.initializeApp({
-  apiKey: "AIzaSyCPKM9TzEC2meM2cM-V1xeR7dvVBrMvwHM",
+  apiKey: "Your API KEY",
   authDomain: "rentup-fe9c0.firebaseapp.com",
-  projectId: "rentup-fe9c0",
+  projectId: "YOUR PROJECT ID",
   storageBucket: "rentup-fe9c0.appspot.com",
-  messagingSenderId: "37191878677",
-  appId: "1:37191878677:web:89a02d169bc18a54cae61c"
+  messagingSenderId: "SENDER ID",
+  appId: "YOUR APP ID"
 });
 
 const messaging = firebase.messaging();
